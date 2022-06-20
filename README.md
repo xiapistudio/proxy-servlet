65cdbb11573f7f3aadc4812e3e0572142db2122b
xieyaowei <xieyaowei@baidu.com>
2022-06-20 17:26:05 +0800

    Init proxy servlet

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # proxy-servlet
+本代码是针对 [Smiley's HTTP Proxy Servlet](https://github.com/mitre/HTTP-Proxy-Servlet) 的进一步改造和尝试。<br>
+
+
---
+++ proxy/src/main/java/com/xiapistudio/http/proxy/ProxyServlet.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2022 Baidu, Inc. All Rights Reserved.
+ */
+
+/**
+ * @class ProxyServlet
+ * @author xieyaowei(xieyaowei@baidu.com)
+ * @date 2022-06-20 17:23
+ * @brief
+ *
+ **/
+
+package com.xiapistudio.http.proxy;
+
+public class ProxyServlet {
+}
